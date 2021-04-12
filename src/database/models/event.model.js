f3f6fb65b5515e1a2d21ad1fccd9171536878045
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-04-12 18:09:00 +0200

    Updated sequelize events table

--- src/database/models/event.model.js
+++ src/database/models/event.model.js
@@ -1,5 +1,7 @@
 import { DataTypes } from 'sequelize';
 
+// TODO : [ ] Check the description size when validating.
+
 export default (sequelize) => {
   sequelize.define(
     'event',
@@ -7,6 +9,7 @@ export default (sequelize) => {
       id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
       start: { type: DataTypes.DATE, allowNull: false },
       link: { type: DataTypes.STRING(255), allowNull: true },
+      description: { type: DataTypes.STRING(255), allowNull: false },
       obligatory: { type: DataTypes.BOOLEAN, allowNull: false },
       created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
       updated_at: { type: DataTypes.DATE, allowNull: true },
