2dd3519c98a62ed9deca12e68d50273aa9fe92ef
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-04-19 14:02:56 +0200

    Set shortcut for role and set a verification for the role users when deleting.

--- src/graphql/role.js
+++ src/graphql/role.js
@@ -1,13 +1,12 @@
 import { AuthenticationError, gql, UserInputError } from 'apollo-server-core';
 
 import errors from '../config/errors';
+import database from '../database';
 
-import { getTableWithUsers } from '../utils/relationMapper';
+import { getObjectWithUsers } from '../utils/relationMapper';
 
 import { checkIsAdmin } from '../utils/authorization';
 import { role as roleShortcut, user as userShortcut } from '../utils/shortcut';
-
-// TODO: [ ] Check if user have the deleted or destroyed role.
 
 export const typeDefs = gql`
   extend type Query {
@@ -41,36 +40,38 @@ export const resolvers = {
   Query: {
     role: async (_parent, args) => {
       const role = await roleShortcut.find(args.id);
-      return getTableWithUsers(role);
+      return getObjectWithUsers(role);
     },
 
     roles: async () => {
       const roles = await roleShortcut.findAll();
-      return roles.map(getTableWithUsers);
+      return roles.map(getObjectWithUsers);
     },
   },
   Mutation: {
     createRole: async (_parent, { input: args }, context) => {
       if (!context?.id) throw new AuthenticationError(errors.NOT_ALLOWED);
 
-      const user = await userShortcut.find(context.id);
-      await checkIsAdmin(user);
+      const loggedUser = await userShortcut.findWithRole(context.id);
+      await checkIsAdmin(loggedUser);
 
       const roleExist = await roleShortcut.findByName(args.role_name);
       if (roleExist) throw new UserInputError(errors.ROLE_DUPLICATION);
 
       const role = await roleShortcut.create({ role_name: args.role_name });
-      return getTableWithUsers(role);
+      return getObjectWithUsers(role);
     },
 
     deleteRole: async (_parent, args, context) => {
       if (!context?.id) throw new AuthenticationError(errors.NOT_ALLOWED);
 
-      const user = await userShortcut.find(context.id);
-      await checkIsAdmin(user);
+      const loggedUser = await userShortcut.findWithRole(context.id);
+      await checkIsAdmin(loggedUser);
 
-      const role = await roleShortcut.find(args.id);
+      const role = await roleShortcut.find(args.id, database.models.user);
       if (!role) throw new UserInputError(errors.DEFAULT);
+
+      if (role?.users.length > 0) throw new UserInputError(errors.ROLE_CASCADE);
 
       await role.update({ deleted_at: null });
       return true;
@@ -79,11 +80,13 @@ export const resolvers = {
     destroyRole: async (_parent, args, context) => {
       if (!context?.id) throw new AuthenticationError(errors.NOT_ALLOWED);
 
-      const user = await userShortcut.find(context.id);
-      await checkIsAdmin(user);
+      const loggedUser = await userShortcut.findWithRole(context.id);
+      await checkIsAdmin(loggedUser);
 
-      const role = await roleShortcut.findDeleted(args.id);
+      const role = await roleShortcut.findDeleted(args.id, database.models.user);
       if (!role) throw new UserInputError(errors.DEFAULT);
+
+      if (role?.users.length > 0) throw new UserInputError(errors.ROLE_CASCADE);
 
       await role.destroy();
       return true;
