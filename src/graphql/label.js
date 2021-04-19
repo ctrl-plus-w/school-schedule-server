ed8745041a2cbf42294118a0743983fe8b3a17c2
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-04-19 14:11:29 +0200

    Set shortcuts for label.

--- src/graphql/label.js
+++ src/graphql/label.js
@@ -1,9 +1,11 @@
-import { gql } from 'apollo-server-core';
+import { AuthenticationError, gql, UserInputError } from 'apollo-server-core';
 
 import errors from '../config/errors';
 import database from '../database';
 
+import { label as labelShortcut, user as userShortcut } from '../utils/shortcut';
 import { getObjectWithUsers } from '../utils/relationMapper';
+import { checkIsAdmin } from '../utils/authorization';
 
 export const typeDefs = gql`
   extend type Query {
@@ -14,11 +16,9 @@ export const typeDefs = gql`
   extend type Mutation {
     createLabel(input: LabelInput!): Label!
 
-    deleteLabelById(user_id: ID!, label_id: ID!): Boolean
-    deleteLabelByName(username: String!, label_name: String!): Boolean
+    deleteLabel(id: ID!): Boolean
 
-    destroyLabelById(user_id: ID!, label_id: ID!): Boolean
-    destroyLabelByName(username: String!, label_name: String!): Boolean
+    destroyLabel(id: ID!): Boolean
   }
 
   input LabelInput {
@@ -38,76 +38,55 @@ export const typeDefs = gql`
 export const resolvers = {
   Query: {
     label: async (_parent, args) => {
-      const label = await database.models.label.findByPk(args.id, { where: { deleted_at: null } });
+      const label = await labelShortcut.find(args.id);
       return getObjectWithUsers(label);
     },
 
     labels: async () => {
-      const labels = await database.models.label.findAll({ where: { deleted_at: null } });
+      const labels = await labelShortcut.findAll();
       return labels.map(getObjectWithUsers);
     },
   },
 
   Mutation: {
-    createLabel: async (_parent, { input: args }) => {
-      const labelExist = await database.models.label.findOne({ where: { label_name: args.label_name, deleted_at: null } });
-      if (labelExist) throw new Error(errors.LABEL_DUPLICATION);
+    createLabel: async (_parent, { input: args }, context) => {
+      if (!context?.id) throw new AuthenticationError(errors.NOT_ALLOWED);
+
+      const user = await userShortcut.find(context.id);
+      await checkIsAdmin(user);
+
+      const labelExist = await labelShortcut.findByName(args.label_name);
+      if (labelExist) throw new UserInputError(errors.LABEL_DUPLICATION);
 
       const label = await database.models.label.create({ label_name: args.label_name /* label_display_name: args.label_display_name */ });
       return getObjectWithUsers(label);
     },
 
-    deleteLabelById: async (_parent, args) => {
-      const user = await database.models.user.findByPk(args.user_id, { where: { deleted_at: null } });
-      if (!user) throw new Error(errors.DEFAULT);
+    deleteLabel: async (_parent, args, context) => {
+      if (!context?.id) throw new AuthenticationError(errors.NOT_ALLOWED);
 
-      const label = await database.models.label.findByPk(args.id, { where: { deleted_at: null } });
-      if (!label) throw new Error(errors.DEFAULT);
+      const user = await userShortcut.find(context.id);
+      await checkIsAdmin(user);
 
-      const labelUsers = await label.getUsers();
-      if (labelUsers.length > 0) throw new Error(errors.LABEL_CASCADE);
+      const label = await labelShortcut.find(args.id, database.models.user);
+      if (!label) throw new UserInputError(errors.DEFAULT);
 
-      await label.update({ deleted_at: Date.now() });
-      return true;
-    },
-
-    deleteLabelByName: async (_parent, args) => {
-      const user = await database.models.user.findOne({ where: { username: args.username, deleted_at: null } });
-      if (!user) throw new Error(errors.DEFAULT);
-
-      const label = await database.models.label.findOne({ where: { label_name: args.label_name, deleted_at: null } });
-      if (!label) throw new Error(errors.DEFAULT);
-
-      const labelUsers = await label.getUsers();
-      if (labelUsers.length > 0) throw new Error(errors.LABEL_CASCADE);
+      if (label?.users.length > 0) throw new UserInputError(errors.LABEL_CASCADE);
 
       await label.update({ deleted_at: Date.now() });
       return true;
     },
 
-    destroyLabelById: async (_parent, args) => {
-      const user = await database.models.user.findByPk(args.user_id, { where: { deleted_at: null } });
-      if (!user) throw new Error(errors.DEFAULT);
+    destroyLabel: async (_parent, args, context) => {
+      if (!context?.id) throw new AuthenticationError(errors.NOT_ALLOWED);
 
-      const label = await database.models.label.findByPk(args.id);
-      if (!label) throw new Error(errors.DEFAULT);
+      const user = await userShortcut.find(context.id);
+      await checkIsAdmin(user);
 
-      const labelUsers = await label.getUsers();
-      if (labelUsers.length > 0) throw new Error(errors.LABEL_CASCADE);
+      const label = await labelShortcut.find(args.id, database.models.user);
+      if (!label) throw new UserInputError(errors.DEFAULT);
 
-      await label.destroy();
-      return true;
-    },
-
-    destroyLabelByName: async (_parent, args) => {
-      const user = await database.models.user.findOne({ where: { username: args.username, deleted_at: null } });
-      if (!user) throw new Error(errors.DEFAULT);
-
-      const label = await database.models.label.findOne({ where: { label_name: args.label_name } });
-      if (!label) throw new Error(errors.DEFAULT);
-
-      const labelUsers = await label.getUsers();
-      if (labelUsers.length > 0) throw new Error(errors.LABEL_CASCADE);
+      if (label?.users.length > 0) throw new UserInputError(errors.LABEL_CASCADE);
 
       await label.destroy();
       return true;
