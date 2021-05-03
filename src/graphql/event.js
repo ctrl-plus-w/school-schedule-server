8280194e7bf299fbdbe96a8d86b95b2366e8bf50
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-05-03 16:37:20 +0200

    Fixed a minor problem.

--- src/graphql/event.js
+++ src/graphql/event.js
@@ -127,7 +127,7 @@ export const resolvers = {
       const usersIdStr = usersId.map((id) => `'${id}'`).join(', ');
       if (usersIdStr.length === 0) return [];
 
-      const labelsSQL = `SELECT Label.id FROM UserLabels JOIN Label ON UserLabels.label_id = Label.id JOIN User ON UserLabels.user_id = User.id WHERE User.id IN(${usersIdStr})`;
+      const labelsSQL = `SELECT Label.id FROM UserLabels JOIN Label ON UserLabels.label_id = Label.id JOIN User ON UserLabels.user_id = User.id WHERE User.id IN(${usersIdStr}) AND User.deleted_at IS NULL`;
       const labels = await database.query(labelsSQL, { type: QueryTypes.SELECT });
 
       const labelsId = labels.map((l) => l.id);
@@ -171,7 +171,7 @@ export const resolvers = {
       const labelUserIds = await label.users.map((user) => user.id);
 
       // SQL Request select id of every user which have an event which start at the given start time.
-      const sql = `SELECT User.id FROM Event JOIN Label ON Event.label_id = Label.id JOIN UserLabels ON UserLabels.label_id = Label.id JOIN User ON UserLabels.user_id = User.id WHERE Event.start = "${startDate.toISOString()}" AND Event.deleted_at IS NULL`;
+      const sql = `SELECT User.id FROM Event JOIN Label ON Event.label_id = Label.id JOIN UserLabels ON UserLabels.label_id = Label.id JOIN User ON UserLabels.user_id = User.id WHERE Event.start = "${startDate.toISOString()} AND Event.deleted_at IS NULL"`;
       const request = await database.query(sql, { type: QueryTypes.SELECT });
 
       const userIdsFromLabels = request.map((user) => user.id);
