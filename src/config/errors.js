135753a6a256b71fdf1dd67bd86f0de2f7e8d68e
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-05-03 16:26:48 +0200

    Added error messages.

--- src/config/errors.js
+++ src/config/errors.js
@@ -4,9 +4,11 @@ export default {
   NOT_LOGGED: `Vous devez être connecté.`,
   NOT_ALLOWED: `Vous n'avez pas la permission de faire ceci.`,
   NOT_EVENT_OWNER: `Vous devez avoir créé cet événement pour le supprimer.`,
+  NOT_SUBJECT_OWNER: `Vous n'avez pas la permission de créer un événement dans cette matière.`,
 
   EVENT_TAKEN: `Vous avez déja un événement à ce moment.`,
   USER_EVENT_TAKEN: `Quelqu'un à déja un événement à ce moment.`,
+  EVENT_IN_PAST: `Vous ne pouvez pas créer un événement dans le passé.`,
 
   USER_DUPLICATION: `Cet utilisateur existe déja.`,
   LABEL_DUPLICATION: `Cet groupe existe déja.`,
@@ -18,4 +20,10 @@ export default {
   ROLE_CASCADE: `Certains utilisateurs sont liés à ce role.`,
 
   BAD_CREDENTIAL: `Le nom d'utilisateur ou le mot de passe est éronné.`,
+
+  SUBJECT_NOT_FOUND: `Veuillez entrer une matière valide.`,
+  ROLE_NOT_FOUND: `Veuillez entrer un rôle valide.`,
+  USER_NOT_FOUND: `Veuillez entrer un utilisateur valide.`,
+  LABEL_NOT_FOUND: `Veuillez entrer un groupe valide.`,
+  EVENT_NOT_FOUND: `L'événement que vous avez entré n'est pas valide.`,
 };
